594d071fce6db6cb26220b07f0afe6e65a8ee80a
Sewen Thy <sewenthy@gmail.com>
2022-09-19 17:07:59 +0800

    change trait function name

--- src/toy_examples/src/extract_to_trait.rs
+++ src/toy_examples/src/extract_to_trait.rs
@@ -1,11 +1,11 @@
 trait MultiLifetimeTrait<'b, 'a: 'b> {
-    fn trait_method(self: &Self, x: & 'a i32, y: &'b i32) -> & 'b i32;
+    fn trait_function(self: &Self, x: & 'a i32, y: &'b i32) -> & 'b i32;
 }
 
 struct SimpleStruct;
 
 impl<'b, 'a: 'b> MultiLifetimeTrait<'b, 'a> for SimpleStruct {
-    fn trait_method(&self, x: &'a i32, y: &'b i32) -> &'b i32 {
+    fn trait_function(&self, x: &'a i32, y: &'b i32) -> &'b i32 {
         if *x < *y {
             y
         } else {
@@ -17,7 +17,7 @@ impl<'b, 'a: 'b> MultiLifetimeTrait<'b, 'a> for SimpleStruct {
 pub fn original_foo<'b, 'a: 'b>(x: &'a i32, y: &'b i32) {
     let foo = SimpleStruct;
     let z = &mut &0;
-    *z = foo.trait_method(x, y);
+    *z = foo.trait_function(x, y);
 }
 
 /*
@@ -28,7 +28,7 @@ pub fn new_foo<'b, 'a: 'b>(x: &'a i32, y: &'b i32) {
 }
 
 fn bar_extracted(x: &i32, y: &i32, foo: SimpleStruct) -> &i32 {
-    foo.trait_method(x, y)
+    foo.trait_function(x, y)
 }
 */
 
@@ -39,5 +39,5 @@ pub fn new_foo_fixed<'b, 'a: 'b>(x: &'a i32, y: &'b i32) {
 }
 
 fn bar_fixed<'b, 'a: 'b>(x: &'a i32, y: &'b i32, foo: SimpleStruct) -> &'b i32 {
-    foo.trait_method(x, y)
+    foo.trait_function(x, y)
 }
