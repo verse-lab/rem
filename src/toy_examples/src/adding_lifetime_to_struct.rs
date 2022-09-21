c1a7c907c2191b675fa952349718a668afa6dadc
Sewen Thy <sewenthy@gmail.com>
2022-09-21 15:53:15 +0800

    updated order

--- src/toy_examples/src/adding_lifetime_to_struct.rs
+++ src/toy_examples/src/adding_lifetime_to_struct.rs
@@ -1,12 +1,13 @@
 #[allow(dead_code)]
 pub struct Foo {x: i32, y: i32}
 
-#[allow(dead_code)]
-pub struct FooUpdated<'a, 'b> {x: &'a i32, y: &'b i32}
-
 pub fn make_foo(x: &i32, y: &i32) -> Foo {
     Foo{x: *x, y: *y}
 }
+
+#[allow(dead_code)]
+pub struct FooUpdated<'a, 'b> {x: &'a i32, y: &'b i32}
+
 pub fn make_foo_updated<'a, 'b>(x: &'a i32, y: &'b i32) -> FooUpdated<'a, 'b>{
     FooUpdated{x, y}
 }
