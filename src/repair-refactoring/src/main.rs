1b69022d015398223adb8b34b2f83db2da0101df
Sewen Thy <sewenthy@gmail.com>
2022-10-11 09:04:22 +0800

    added timing

--- src/repair-refactoring/src/main.rs
+++ src/repair-refactoring/src/main.rs
@@ -1,13 +1,17 @@
+use std::time::SystemTime;
+
 mod repair_lifetime_simple;
 
 fn main() {
     let file_names = vec!["borrow", "in_out_lifetimes", "lifetime_bounds", "in_out_lifetimes_original_extract"];
     for file_name in file_names {
+        let now = SystemTime::now();
         let success =
             repair_lifetime_simple::repair_file(
                 format!("./input/{}.rs", file_name).as_str(),
                 format!("./output/{}.rs", file_name).as_str()
             );
-        println!("refactored {}: {}", file_name, success);
+        let time_elapsed = now.elapsed().unwrap();
+        println!("refactored {} success: {} in {:#?}", file_name, success, time_elapsed);
     }
 }
