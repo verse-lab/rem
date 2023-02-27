31bfe9458c7261fc4758cecafbfd5c373d425fdd
Sewen Thy <me@sewenthy.dev>
2023-02-27 22:36:24 +0800

    use structs for expr

--- src/experiment/src/utils.rs
+++ src/experiment/src/utils.rs
@@ -1,13 +1,13 @@
 use std::process::Command;
 
-pub fn checkout(dir: String, branch: String) -> bool {
+pub fn checkout(dir: &String, branch: &String) -> bool {
     let mut checkout = Command::new("git");
     checkout.arg("-C").arg(dir).arg("checkout").arg(branch);
     let out = checkout.output().unwrap();
     out.status.success()
 }
 
-pub fn checkout_b(dir: String, branch: String) -> bool {
+pub fn checkout_b(dir: &String, branch: &String) -> bool {
     let mut checkout = Command::new("git");
     checkout
         .arg("-C")
